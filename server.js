b4ea8d7285eaa8b8e22074c3a84070e75321a63e
David Trejo <david.daniel.trejo@gmail.com>
2010-10-02 23:04:17 -0400

    added fall-apart 404 page

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-var static = require('node-static');
+var static = require('node-static')
+  , http = require('http')
+  
+  // Create a node-static server instance to serve the './public' folder
+  , fileServer = new static.Server('./public');
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var file = new(static.Server)('./public');
+http.createServer(function (request, response) {
+  request.addListener('end', function () {
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
+    // Serve files!
+    fileServer.serve(request, response, function (e, res) {
+      if (e && (e.status === 404)) { // If the file wasn't found
+        // fileServer.serveFile('404.html', request, response);
+        fileServer.serveFile('404.html', 404, {}, request, response);
+      }
     });
+  });
 }).listen(80);
